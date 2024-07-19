bca2f60204f887d207cbe02871b74fe10cb9294f
Ayihlome <ayihlome12@gmail.com>
2024-07-19 09:27:07 +0200

    Refactoring
    I put everything into a function for better readability and usability.

--- LuckyNumber.py
+++ LuckyNumber.py
@@ -1,28 +1,31 @@
 import random
 number = int(input(" Guess the lucky number between 1 to 100: "))
-luckyNumber = random.randrange(1,100)
 guesses = 1
 correct = False
+quary = str
+redo = bool
 
-# print(luckyNumber)
 
 # testing the numbers if they are correct
-while not correct:
-    if number > 100 or number < 0:
+def TestNumber (number):
+     correct = False
+     guesses = 1
+     actualNumber = random.randrange(1,100)
+     print (actualNumber)
+     while not correct:
+      if number > 100 or number < 0:
          number = int(input("The number is not in range, Please try again: "))
          guesses += 1
-    elif number > luckyNumber: 
+      elif number > actualNumber: 
          number = int(input("Number is too high, guess again: "))
          guesses += 1
-    elif number < luckyNumber:
+      elif number < actualNumber:
          number = int(input("Number is too low, guess again: "))
          guesses += 1
-    elif number == luckyNumber:
+      elif number == actualNumber:
          correct = True
          print("CORRECT!!!!!")
          print("You did it in " + str(guesses))
-      
-    
-     
+# end function
 
-
+TestNumber(number)
